fdd2b5edb38ca5f19107b21b3cd5bd1cc43cee56
ridwan <ridwan@softwareseni.com>
2019-07-15 13:04:32 +0700

    psuh mbs

---
+++ mbs/precache-manifest.9dab8eefdb60cae3324538e1aef9a19b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "cea23e620e1b514d5181",
+    "url": "/static/css/main.922c94ce.chunk.css"
+  },
+  {
+    "revision": "cea23e620e1b514d5181",
+    "url": "/static/js/main.cea23e62.chunk.js"
+  },
+  {
+    "revision": "8b0395d468725bd682b1",
+    "url": "/static/css/1.c2289b4a.chunk.css"
+  },
+  {
+    "revision": "8b0395d468725bd682b1",
+    "url": "/static/js/1.8b0395d4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b02972af9316dd3c8813151066f33a06",
+    "url": "/static/media/noimage.b02972af.jpg"
+  },
+  {
+    "revision": "b2ea7e6424f47e20fb7a9c3166019bf2",
+    "url": "/static/media/logo.b2ea7e64.png"
+  },
+  {
+    "revision": "8818b04f085ab6ac2fd9550386d2c59c",
+    "url": "/static/media/logo-white.8818b04f.png"
+  },
+  {
+    "revision": "a72c74796024918b061e866529bbd1a1",
+    "url": "/static/media/home_banner.a72c7479.jpg"
+  },
+  {
+    "revision": "ff3ea0cd2f91c5992278e246c71abfbe",
+    "url": "/static/media/connection.ff3ea0cd.png"
+  },
+  {
+    "revision": "500ee7a9762ca06407dcbf43f47ced97",
+    "url": "/static/media/back_trans.500ee7a9.jpg"
+  },
+  {
+    "revision": "95a9004f47adcea92fab734006d466a8",
+    "url": "/static/media/investor.95a9004f.jpg"
+  },
+  {
+    "revision": "6eb70160b03c84dd72846220742e0382",
+    "url": "/static/media/team.6eb70160.jpg"
+  },
+  {
+    "revision": "07ed319f8a5c982fb3e55a738dcea5e6",
+    "url": "/static/media/point.07ed319f.png"
+  },
+  {
+    "revision": "d88a8ae6c849e05d943d70ef1dca40de",
+    "url": "/static/media/about.d88a8ae6.png"
+  },
+  {
+    "revision": "6d7ce2a2c2af067707d13c661e744359",
+    "url": "/static/media/visi.6d7ce2a2.png"
+  },
+  {
+    "revision": "1eb9ee3c36b0fd9230cc90189beb027e",
+    "url": "/static/media/call-center.1eb9ee3c.png"
+  },
+  {
+    "revision": "d58df59bb29d00aee81c1152a9ffb4db",
+    "url": "/static/media/Somatic-Rounded.d58df59b.woff2"
+  },
+  {
+    "revision": "40fb5120ba1abe38469c12f0d7c990ca",
+    "url": "/index.html"
+  }
+];
